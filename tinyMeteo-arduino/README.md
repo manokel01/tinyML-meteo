590af774cbe209d8da1b949d7209c9bb8dfeda42
manokel01 <manokel@gmail.com>
2023-05-20 21:09:48 +0300

    updated arduino with json

--- tinyMeteo-arduino/README.md
+++ tinyMeteo-arduino/README.md
@@ -1,5 +1,24 @@
-## Arduino Installation
+# tinyML on an Arduino
 
-The model.h created py the [Python code](https://github.com/manokel01/tinyML-meteo/tree/main/python_model) has to be saved in same folder as Arduino sketch. 
+## Overview
+tinyML on an Arduino Nano 33 Sense measuring weather data with its sensors and making live weather predictions
+using a Dense Neural Netowrk (DNN) model trained with TensorFlow. The data is send over serial port in JSON format.
+Sensors:
+- temperature (±0.1°C)/ HS3003 sensor
+- humidity (%RH)/ HS3003 sensor
+- barometric pressure (260-1260 kPa) / LPS22HB sensor
 
-The sketch uses a fork of the Arduino Array Library created by Alexander Brevig and updated to work with the more recent versions of Arduino by [Baptiste Zioch](https://github.com/BaptisteZloch).
+## Dependencies:
+- ELoquentTinyML.h
+- eloquent_tinyml/tensorflow.h
+- ArduinoJson.h
+- Array.h (The sketch uses a fork of the Arduino Array Library created by Alexander Brevig and updated to work with the more recent versions of Arduino by [Baptiste Zioch](https://github.com/BaptisteZloch).
+Make sure to include your trained and converted with TFLite model file in the same folder as your Arduino sketch.
+
+ ## Installation
+
+After creating the model and converting with TensorFLow Lite copy the file ("model.h) in same folder as Arduino sketch.
+
+Upload sketch (.ino file) to the Arduino using the Arduino IDE.
+
+
--- tinyMeteo-arduino/tinyMeteo-arduino.ino
+++ tinyMeteo-arduino/tinyMeteo-arduino.ino
@@ -1,13 +1,41 @@
+/* 
+ * tinyML on an Arduino Nano 33 Sense measuring weather data 
+ * with its sensors and making live weather predictions
+ * using a Dense Neural Netowrk (DNN) model trained with
+ * TensorFlow.
+ * The data is send over serial port in JSON format.
+ * Sensors:
+ * - temperature (±0.1°C)/ HS3003 sensor
+ * - humidity (%RH)/ HS3003 sensor
+ * - barometric pressure (260-1260 kPa) / LPS22HB sensor
+ * Dependencies:
+ * - ELoquentTinyML.h and eloquent_tinyml/tensorflow.h
+ * - Array.h (modified version included in this repo)
+ * - ArduinoJson.h
+ * Make sure to include your trained and converted with TFLite model file
+ * in the same folder as your Arduino sketch.
+ *
+ * Version: 1.0
+ * Author: Manolis Kelaidis
+ * Date: May 2023
+ */
+ 
 #include <Arduino_LPS22HB.h>
 #include <Arduino_HS300x.h>
 #include <EloquentTinyML.h>
 #include "eloquent_tinyml/tensorflow.h"
 #include <Array.h>
 #include "model.h"
+#include <ArduinoJson.h>
 
 #define NUMBER_OF_INPUTS 3
 #define NUMBER_OF_OUTPUTS 5
 #define TENSOR_ARENA_SIZE 4*1024
+#define LOCATION_ID 14320000
+#define VENDOR_ID 2341
+#define PRODUCT_ID "805a"
+
+StaticJsonDocument<256> doc;
 
 Eloquent::TinyML::TensorFlow::TensorFlow<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml;
 
@@ -35,14 +63,27 @@ void loop() {
   float output[NUMBER_OF_OUTPUTS] = {0, 0, 0, 0, 0};
   ml.predict(input, output);
 
-  Serial.print("Temperature : ");
-  Serial.print(temperature);
-  Serial.print(" °C - Humidity : ");
-  Serial.print(humidity);
-  Serial.print(" % - Pressure : ");
-  Serial.println(pressure);
-  Serial.print("The weather is : ");
-  Serial.println(value(output));
+  // Serial.print("Temperature : ");
+  // Serial.print(temperature);
+  // Serial.print(" °C - Humidity : ");
+  // Serial.print(humidity);
+  // Serial.print(" % - Pressure : ");
+  // Serial.println(pressure);
+  // Serial.print("The weather is : ");
+  // Serial.println(value(output));
+
+  // serialize stream to JSON
+  JsonObject root = doc.to<JsonObject>();
+  root["temperature"] = temperature;
+  root["humidity"] = humidity;
+  root["pressure"] = pressure;
+  root["weatherDesc"] = value(output);
+  root["locationID"] = LOCATION_ID;
+  root["vendorID"] = VENDOR_ID;
+  root["productID"] = PRODUCT_ID;
+  serializeJson(root, Serial);
+  Serial.println();
+
   delay(1000);
 }
 
